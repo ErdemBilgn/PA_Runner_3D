bb4231ff0d2642de81f5d4c0a3a5150a2a4f5bad
Wise2nd <mr.erdembilgin@gmail.com>
2023-06-07 20:37:16 +0300

    Added Character

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float _runSpeed;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + _runSpeed * Time.deltaTime);
+        transform.position = newPos;
+    }
+}

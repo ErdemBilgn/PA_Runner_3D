ec0104b2e0b5a4af280b52cc1ac1922569c10aec
Wise2nd <mr.erdembilgin@gmail.com>
2023-06-12 20:30:23 +0300

    Added Swipe Check

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float _runSpeed;
+    [SerializeField] float _xSpeed;
+    [SerializeField] float _limitX;
+
     void Start()
     {
         
@@ -13,7 +16,27 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z + _runSpeed * Time.deltaTime);
+        SwipeCheck();
+    }
+
+    void SwipeCheck()
+    {
+        float newX = 0;
+        float touchXDelta = 0;
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            //Debug.Log(Input.GetTouch(0).deltaPosition.x / Screen.width);
+            touchXDelta = Input.GetTouch(0).deltaPosition.x / Screen.width;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            touchXDelta = Input.GetAxis("Mouse X");
+        }
+
+        newX = transform.position.x + _xSpeed * touchXDelta * Time.deltaTime;
+        newX = Mathf.Clamp(newX, -_limitX, _limitX);
+
+        Vector3 newPos = new Vector3(newX, transform.position.y, transform.position.z + _runSpeed * Time.deltaTime);
         transform.position = newPos;
     }
 }
